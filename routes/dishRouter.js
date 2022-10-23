40cd6c9b1549deb274593d0d1c8eaa250e924eda
karanagrawal14 <karanagrawal1411@gmail.com>
2022-10-23 09:46:11 +0530

    openssl

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -21,7 +21,7 @@ dishRouter.route('/')
     },err=>next(err))
     .catch(err=>next(err));
 })
-.post(authenticate.verifyUser,(req, res, next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Dishes.create(req.body)
     .then((dish)=>{
         console.log('Dish Created ',dish);
@@ -32,11 +32,11 @@ dishRouter.route('/')
     },err=>next(err))
     .catch(err=>next(err));
 })
-.put(authenticate.verifyUser,(req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /dishes');
 })
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Dishes.remove({})
     .then((resp)=>{
         res.statusCode = 200;
@@ -57,11 +57,11 @@ dishRouter.route('/:dishId')
         res.json(dish);
     },err=>next(err));
 })
-.post(authenticate.verifyUser,(req, res, next) => {
+.post(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     res.statusCode = 403;
       res.end('POST operation not supported on /dishes/'+ req.params.dishId);
 })
-.put(authenticate.verifyUser,(req, res, next) => {
+.put(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Dishes.findByIdAndUpdate(req.params.dishId,{$set:req.body},{new:true})
     .then((dish)=>{
         res.statusCode=200;
@@ -72,7 +72,7 @@ dishRouter.route('/:dishId')
     .catch(err=>next(err))
 })
     
-.delete(authenticate.verifyUser,(req, res, next) => {
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req, res, next) => {
     Dishes.findByIdAndRemove(req.params.dishId)
     .then((resp)=>{
         res.statusCode=200;
@@ -134,7 +134,7 @@ dishRouter.route('/:dishId/comments')
     res.end('PUT operation not supported on /dishes/'+req.params.dishId+'/comments');
 
 })
-.delete(authenticate.verifyUser,(req,res,next)=>{
+.delete(authenticate.verifyUser,authenticate.verifyAdmin,(req,res,next)=>{
     Dishes.findById(req.params.dishId)
     .then((dish)=>{
         if(dish!=null)
@@ -190,6 +190,12 @@ dishRouter.route('/:dishId/comments/:commentId')
     Dishes.findById(req.params.dishId)
     .then((dish)=>{
         if(dish!=null&&dish.comments.id(req.params.commentId)!=null){
+            if(req.user._id.toString()!=dish.comments.id(req.params.commentId).author.toString())
+            {
+                var err = new Error('You are not authenticated to edit this comment');
+                err.status = 403;
+                return next(err);
+            }
             if(req.body.rating){
                 dish.comments.id(req.params.commentId).rating=req.body.rating;
 
@@ -226,6 +232,12 @@ dishRouter.route('/:dishId/comments/:commentId')
     .then((dish)=>{
         if(dish!=null && dish.comments.id(req.params.commentId)!=null)
         {
+            if(req.user._id.toString()!=dish.comments.id(req.params.commentId).author.toString())
+            {
+                var err = new Error('You are not authenticated to edit this comment');
+                err.status = 403;
+                return next(err);
+            }
             dish.comments.id(req.params.commentId).remove();
             dish.save()
             .then((dish)=>{
